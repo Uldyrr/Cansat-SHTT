6a6864ab6a7cfb51b26479e8ed0ba0518534ea4c
ManexXd <74118310+ManexXd@users.noreply.github.com>
2023-11-08 16:59:25 +0100

    Updated with alarm buzzer test code

--- Raspberry Pi Pico/src/main.py
+++ Raspberry Pi Pico/src/main.py
@@ -5,15 +5,16 @@ from bmp280 import *
 from micropyGPS import MicropyGPS
 import time
 
-
 # Values
+alarmBuzzerDebounce: bool = False
+
 # // Sensors
 mpu: MPU6050 = MPU6050(I2C(1, sda=Pin(26), scl=Pin(27)))
-bmp: BMP280 = BMP280(I2C(1, sda=Pin(26), scl=Pin(27), freq=200000))
+bmp: BMP280 = BMP280(I2C(1, sda=Pin(26), scl=Pin(27)))
 
 # // Components
-loRaSerialBus: UART = UART(1, 9600, tx = Pin(8), rx = Pin(9))
-gpsSerialBus: UART = UART(1, 9600, tx = Pin(4), rx = Pin(5))
+loRaSerialBus: UART = UART(1, 9600, tx=Pin(8), rx=Pin(9))
+gpsSerialBus: UART = UART(1, 9600, tx=Pin(4), rx=Pin(5))
 gps: MicropyGPS = MicropyGPS()
 
 # // Sensor data
@@ -22,24 +23,32 @@ mpuData: dict = {}
 
 # Init
 def Init():
-    bmp.use_case(BMP280_CASE_INDOOR) # BMP280 Pressure measurement setting
+    bmp.use_case(BMP280_CASE_INDOOR)  # Innendørs
 
 
 # The heart of the CanSat
 def MainCycle():
+    global alarmBuzzerDebounce
+
     while True:
-        accelerationData, gyroData, airTemperatureData = GetMPUAccelerationGyroTemp(mpu, mpuData)
+        accelerationData, gyroData, airTemperatureData = GetMPUAccelerationGyroTemp(mpu, bmp, mpuData)
         airPressureData, altitudeData = GetBMPPressureAltitude(bmp, airTemperatureData)
         gpsLatitude, gpsLongitude = GetGPSLatitudeLongitude(gps, gpsSerialBus)
 
-        #print(mpuData)
+        # print(mpuData)
         print(f"Latitude: {gpsLatitude}, Longitude: {gpsLongitude}")
-        #print("Air Temperature: {}C".format(airTemperatureData))
-        #print("Pressure: {} Pa".format(airPressureData))
-        #print("Altitude:", altitudeData)
+        # print("Air Temperature: {}C".format(airTemperatureData))
+        # print("Pressure: {} Pa".format(airPressureData))
+        # print("Altitude:", altitudeData)
+
+        alarmBuzzerDebounce = not alarmBuzzerDebounce
+
+        SetAlarmBuzzerState(alarmBuzzerDebounce)
 
         time.sleep(CANSAT_UPDATEHZ)
 
 
 Init()
 MainCycle()
+
+
