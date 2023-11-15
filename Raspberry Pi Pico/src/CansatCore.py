fd49af04f7bdc4d6edef95be7ec82ec7a7c0c5f9
ManexXd <74118310+ManexXd@users.noreply.github.com>
2023-11-15 23:17:04 +0100

    Update CansatCore.py

--- Raspberry Pi Pico/src/CansatCore.py
+++ Raspberry Pi Pico/src/CansatCore.py
@@ -1,33 +1,38 @@
-import utime
-import _thread
 from machine import Pin, ADC, UART
 from imu import MPU6050
 from bmp280 import BMP280
 from micropyGPS import MicropyGPS
 from lora_e32 import LoRaE32
+from lora_e32_operation_constant import ResponseStatusCode
 from vector3d import Vector3d
+import utime
+import _thread
 
 
 # Module values
 # // Values
-__alarmBuzzerRunning: bool = False
+__alarmBuzzerRunning: bool = False  # Used in relation with the cansat's alarm buzzer
 
 # // Sensors
 __builtInTemperatureSensor: ADC = ADC(4)
+
+# // Components
+__builtInLed: Pin = Pin(25, Pin.OUT)
 __alarmBuzzer: Pin = Pin(20, Pin.OUT)
-# TODO: Add builtin led code!
 
 # // Constants
 CANSAT_UPDATEHZ: float = 1  # Hz
 CANSAT_ALTITUDECORRECTION: float = 120.0  # M
+CANSAT_SEALEVELPRESSURE: float = 1013.25  # hPa
 
-PRESSURE_SEALEVEL: float = 1013.25  # hPa
-
-LORA_ADDRESS: int = 0x6  # The two seperate int8 (byte) addresses of the reciever that is supposed to acquire the message
-LORA_CHANNEL: int = 0x17  # The channel at which to send the message (signal) at
+LORA_ADDRESS: int = 0x5  # An int8 (byte) address of the reciever that is supposed to acquire the message in Fixedmode
+LORA_CHANNEL: int = 0x17  # The channel (Frequency) at which to send the message at
 
 BUZZER_ALARM_HZ: int = 800  # The frequency at which the buzzer will play
 BUZZER_MICROSECONDS: int = 1000000  # 1 / 1000 (ms) / 1000 (us)
+
+HIGH: bool = True
+LOW: bool = False
 
 
 def GetBuiltInTemperature() -> float:
@@ -44,7 +49,7 @@ def GetBuiltInTemperature() -> float:
     return round(temperature, 2)
 
 
-def GetAltitude(airPressurehPa: float, airTemperatureKelvin: float) -> float:
+def GetHypsometricAltitude(airPressurehPa: float, airTemperatureKelvin: float) -> float:
     """
     Gets the current altitude using the Hypsometric equation
 
@@ -55,19 +60,19 @@ def GetAltitude(airPressurehPa: float, airTemperatureKelvin: float) -> float:
     :return: Returns a float of the current altitude of the cansat based on the given air pressure and temperature
     """
 
-    pressureRatio: float = PRESSURE_SEALEVEL / airPressurehPa  # sea level pressure = 1013.25 hPa
+    pressureRatio: float = CANSAT_SEALEVELPRESSURE / airPressurehPa
     altitude: float = (((pressureRatio ** (1 / 5.257)) - 1) * airTemperatureKelvin) / 0.0065 - CANSAT_ALTITUDECORRECTION
 
     return altitude
 
 
-def GetMPUAccelerationGyroTemp(mpu: MPU6050, bmp: BMP280, mpuData: dict) -> tuple[Vector3d, Vector3d, float]:
+def GetMPUAccelerationGyroTemp(mpu: MPU6050, bmp: BMP280, mpuData: dict = None) -> tuple[Vector3d, Vector3d, float]:
     """
     Gets the current acceleration, gyro, and temperature data using MPU6050 and BMP280
 
     :param MPU6050 mpu: The mpu6050 sensor connected to the cansat
     :param BMP280 bmp: The bmp280 sensor connected to the cansat
-    :param dict mpuData: A dictionary that will be updated to the mpu and bmp's data with keys "Acceleration", "Gyroscope", "Temperature"
+    :param dict? mpuData: A dictionary that when passed will be updated with both the mpu's and bmp's data with the given keys: "Acceleration", "Gyroscope", "Temperature"
     :return: Returns the acceleration, gyroscope, and air temperature data
     """
 
@@ -75,20 +80,21 @@ def GetMPUAccelerationGyroTemp(mpu: MPU6050, bmp: BMP280, mpuData: dict) -> tupl
     gyroData: Vector3d = mpu.gyro
     airTemperatureData: float = bmp.temperature
 
-    # Update the mpuData dict with MPU6050 data
-    mpuData["Acceleration"] = {
-        "x": accelerationData.x,
-        "y": accelerationData.y,
-        "z": accelerationData.z
-    }
+    # Update the mpuData dict (IF PROVIDED) with data from the MPU6050
+    if type(mpuData) is dict:
+        mpuData["Acceleration"] = {
+            "x": accelerationData.x,
+            "y": accelerationData.y,
+            "z": accelerationData.z
+        }
 
-    mpuData["Gyroscope"] = {
-        "x": gyroData.x,
-        "y": gyroData.y,
-        "z": gyroData.z
-    }
+        mpuData["Gyroscope"] = {
+            "x": gyroData.x,
+            "y": gyroData.y,
+            "z": gyroData.z
+        }
 
-    mpuData["Temperature"] = airTemperatureData
+        mpuData["Temperature"] = airTemperatureData
 
     return accelerationData, gyroData, airTemperatureData
 
@@ -107,7 +113,7 @@ def GetBMPPressureAltitude(bmp: BMP280, airTemperature: float) -> tuple[float, f
     hpaAirPressure: float = airPressureData * 0.01  # hPa
     airTemperatureKelvin: float = airTemperature + 273.15  # Kelvin
 
-    altitudeData: float = GetAltitude(hpaAirPressure, airTemperatureKelvin)
+    altitudeData: float = GetHypsometricAltitude(hpaAirPressure, airTemperatureKelvin)
 
     return airPressureData, altitudeData
 
@@ -131,24 +137,40 @@ def GetGPSLatitudeLongitude(gps: MicropyGPS, gpsSerialBus: UART) -> tuple[list, 
     return gps.latitude, gps.longitude
 
 
-def LoRaSendMessage(loRa: LoRaE32, message: dict):
+def LoRaSendDictData(loRa: LoRaE32, dictData: dict) -> str:
     """
-    Sends a radio message using a long range (LoRa) radio component in Fixedmode
+    Sends a dictionary through radio using a long range (LoRa) radio component in Fixedmode
 
-    :param LoRaE32 loRa: The long range (LoRa) radio component to use
-    :param dict message: The dictionary message to send to a long range (LoRa) radio component reciever
+    :param LoRaE32 loRa: The long range (LoRa) radio component to use with transmitting the data
+    :param dict dictData: The dictionary data to send over to a long range (LoRa) radio component reciever
+    :return: The string ResponseStatusCode enum code
     """
 
-    loRa.send_fixed_dict(0, LORA_ADDRESS, LORA_CHANNEL, message)
+    return ResponseStatusCode.get_description(loRa.send_fixed_dict(0, LORA_ADDRESS, LORA_CHANNEL, dictData))
+
+
+
+def ToggleBuiltInLed(ledState: bool = None):
+    """
+    Toggles the Raspberry Pi Pico's built-in LED on or off
+
+    :param bool ledState: A boolean value expressing the power state of the built-in LED
+    """
+
+    # Inverse the current power state of the built-in LED
+    if ledState is None:
+        ledState = not __builtInLed.value()
+
+    __builtInLed.value(ledState)
 
 
 def __AlarmBuzzerUpdate():
     """
-    Toggles the power of the alarm buzzer in a fashion that creates a sound with a particular frequency.
+    Toggles the power of the alarm buzzer in a fashion that creates a sound with a particular frequency
 
     .. note:: The function's while loop update will stop when the alarm buzzer's running state is set to False
 
-    .. seealso:: SetAlarmBuzzerState(alarmState: bool)
+    .. seealso:: ToggleAlarmBuzzerState(alarmState: bool)
     """
 
     global __alarmBuzzerRunning, __alarmBuzzer
@@ -163,16 +185,20 @@ def __AlarmBuzzerUpdate():
         utime.sleep_us(alarmBuzzerSleepTime)
 
 
-def SetAlarmBuzzerState(alarmState: bool):
+def ToggleAlarmBuzzerState(alarmState: bool = None):
     """
-    Sets the running state of the cansat's alarm buzzer to a given state.
+    Toggles the running state of the cansat's alarm buzzer to a given state
 
-    .. note:: This function creates a new thread when the running state is true!
+    .. note:: This function will create a new thread to operate on when the alarm state is true!
 
-    :param bool alarmState: The boolean value the running state of the alarm buzzer will be set to
+    :param bool alarmState: A boolean value expressing the running state of the cansat's alarm buzzer. When None, the function will simply inverse the current running state
     """
 
     global __alarmBuzzerRunning
+
+    # Inverse the current running state of the alarm buzzer
+    if alarmState is None:
+        alarmState = not __alarmBuzzerRunning
 
     if alarmState and __alarmBuzzerRunning is False:
         __alarmBuzzerRunning = True
