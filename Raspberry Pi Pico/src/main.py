8b41abf94dff042b49dcc847dd5407bb01f64b9f
ManexXd <74118310+ManexXd@users.noreply.github.com>
2023-11-15 23:18:22 +0100

    Update main.py

--- Raspberry Pi Pico/src/main.py
+++ Raspberry Pi Pico/src/main.py
@@ -4,11 +4,12 @@ from imu import MPU6050
 from bmp280 import *
 from micropyGPS import MicropyGPS
 from lora_e32 import LoRaE32
-import time
+from lora_e32_operation_constant import ResponseStatusCode
+import _thread
+import utime
+
 
 # Values
-alarmBuzzerDebounce: bool = False
-
 # // Sensors
 # mpu: MPU6050 = MPU6050(I2C(1, sda=Pin(26), scl=Pin(27)))
 # bmp: BMP280 = BMP280(I2C(1, sda=Pin(26), scl=Pin(27)))
@@ -18,44 +19,35 @@ alarmBuzzerDebounce: bool = False
 # gps: MicropyGPS = MicropyGPS()
 loRaSerialBus: UART = UART(0, tx=Pin(16), rx=Pin(17))
 loRa: LoRaE32 = LoRaE32("433T30D", loRaSerialBus)
-builtInLed: Pin = Pin(25, Pin.OUT)
+loRaAux: Pin = Pin(18, Pin.IN)
 
 # // Sensor data
 mpuData: dict = {}
 
 
-# Init
-# def Init():
-# bmp.use_case(BMP280_CASE_INDOOR)  # Innendørs
+# Toggles the built-in LED based off of available data
+def LoRaAuxUpdate():
+    while True:
+        ToggleBuiltInLed(loRaAux.value())
 
 
 # The heart of the CanSat
 def MainCycle():
-    global alarmBuzzerDebounce
-
     while True:
         # accelerationData, gyroData, airTemperatureData = GetMPUAccelerationGyroTemp(mpu, bmp, mpuData)
         # airPressureData, altitudeData = GetBMPPressureAltitude(bmp, airTemperatureData)
         # gpsLatitude, gpsLongitude = GetGPSLatitudeLongitude(gps, gpsSerialBus)
 
-        # print(mpuData)
-        # print(f"Latitude: {gpsLatitude}, Longitude: {gpsLongitude}")
-        # print("Air Temperature: {}C".format(airTemperatureData))
-        # print("Pressure: {} Pa".format(airPressureData))
-        # print("Altitude:", altitudeData)
+        print(LoRaSendDictData(loRa, {'temp': 20, 'lufttrykk': 1000}))
 
-        alarmBuzzerDebounce = not alarmBuzzerDebounce
-
-        # SetAlarmBuzzerState(alarmBuzzerDebounce)
-
-        LoRaSendMessage(loRa, {'temp': 20, 'lufttrykk': 1000})
-
-        builtInLed.value(1)
-        time.sleep(CANSAT_UPDATEHZ/10)
-        builtInLed.value(0)
-        time.sleep(CANSAT_UPDATEHZ - CANSAT_UPDATEHZ/10)
+        utime.sleep(CANSAT_UPDATEHZ)
 
 
-# Init()
+def Init():
+    _thread.start_new_thread(LoRaAuxUpdate, ())
+
+    # bmp.use_case(BMP280_CASE_INDOOR)  # Innendørs
+
+
+Init()
 MainCycle()
-
