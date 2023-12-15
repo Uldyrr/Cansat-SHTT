36d320ddb319868dc3ae29e3428ecdc033ba4e81
tommy <tommyroslik420@gmail.com>
2023-12-15 09:32:58 +0100

    Update

--- Raspberry Pi Pico/src/CansatLogger.py
+++ Raspberry Pi Pico/src/CansatLogger.py
@@ -21,7 +21,7 @@ class CansatLogger:
     def LogData(self, data: str):
         self._logCount += 1
 
-        data = "".join(data.splitlines())#fjeijfei
+        data = "".join(data.splitlines())
 
         with open(self._logName, "a") as logFile:
             logFile.write(f"ID: {self._logCount} | Data: {data}\n")
