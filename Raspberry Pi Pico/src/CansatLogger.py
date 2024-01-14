b3b0edf49946874a74047639189dba91cf606c14
tommy <tommyroslik420@gmail.com>
2024-01-14 19:10:48 +0100

    Update

--- Raspberry Pi Pico/src/CansatLogger.py
+++ Raspberry Pi Pico/src/CansatLogger.py
@@ -7,6 +7,9 @@ LOGGER_LOGMAINNAME = "DataLog"
 LOGGER_LOGIMUNAME = "IMULog"
 LOGGER_LOGFOLDER = "Logs"
 
+LOGTYPE_MAINDATA = "Main"
+LOGTYPE_IMUDATA = "IMU"
+
 # // Paths
 _workingDirectoryPath = os.getcwd()
 _logFolderPath = f"{_workingDirectoryPath}{LOGGER_LOGFOLDER}/"
@@ -17,17 +20,19 @@ class CansatLogger:
     _logIMUName = ""
 
     def __init__(self):
-        logDataID = len(os.listdir(_logFolderPath)) + 1
+        self._logMainName = f"{_logFolderPath}{LOGGER_LOGMAINNAME}{len(os.listdir(_logFolderPath))}.csv"
+        self._logIMUName = f"{LOGGER_LOGIMUNAME}.csv"
 
-        self._logMainName = f"{_logFolderPath}{LOGGER_LOGMAINNAME}{logDataID}.csv"
-        self._logIMUName = f"{_logFolderPath}{LOGGER_LOGIMUNAME}{logDataID}.csv"
+        # Reset IMU data every restart
+        with open(self._logIMUName, "x") as logIMUFile:
+            logIMUFile.write("")
 
-    def LogData(self, *data: any):
+    def LogData(self, logType: str, *data: any):
         logStringData = ""
 
         for i in range(0, len(data) - 1):
             logStringData += str(data[i]) + ("\n" if i == len(data) - 1 else ";")
 
-        with open(self._logName, "a") as logFile:
+        with open(self._logMainName if logType == LOGTYPE_MAINDATA else self._logIMUName, "a") as logFile:
             logFile.write(logStringData)
 
