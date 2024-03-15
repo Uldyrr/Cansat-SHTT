5e18d421143b3d9cd7247509f59cb237a81278c1
tommy <tommyroslik420@gmail.com>
2024-03-15 09:29:56 +0100

    Update

---
+++ Raspberry Pi Pico/src/CansatResistance.py
@@ -0,0 +1,8 @@
+class EarthResistanceSensor:
+    def __init__(self):
+        print("Henrik do your thing :D")
+
+    def Measure(self) -> int:
+        print("Henrik pls implement")
+
+        return -1
