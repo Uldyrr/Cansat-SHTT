8fa07f6eb3fdfd008b78d641a5100066690caaa8
Henrik <henrik.gullstein@hotmail.com>
2024-03-22 09:03:15 +0100

    yes

--- Raspberry Pi Pico/src/CansatSoil.py
+++ Raspberry Pi Pico/src/CansatSoil.py
@@ -1,8 +1,15 @@
 # Henrik, do your thing :D
+from machine import Pin, ADC
+
 
 class SoilResistanceSensor:
     def __init__(self, adcPin: int):
-        return
+        self._adc = ADC(adcPin)
+        self.voltage_in = 3.3  # Voltage going into sensor
+        self.reference_resistance = 100000
 
     def MeasureResistance(self) -> int:
-        return -1
+        voltage_out = self._adc.read_u16()
+        buffer = self.voltage_in / voltage_out - 1
+        measured_resistance = self.reference_resistance * buffer
+        return measured_resistance
