eb530d98b8c0c8dcbbc932349975f34425dff1d6
tommy <tommyroslik420@gmail.com>
2024-03-26 15:49:27 +0100

    Update

--- Raspberry Pi Pico/src/CansatLogger.py
+++ Raspberry Pi Pico/src/CansatLogger.py
@@ -4,6 +4,7 @@ import os
 # // Constants
 LOGGER_LOGMAINNAME: str = "DataLog"
 LOGGER_LOGFOLDER: str = "Logs"
+LOGGER_CSVSEPERATOR: str = ";"
 
 # // Paths
 _workingDirectoryPath: str = os.getcwd()
@@ -11,19 +12,16 @@ _logFolderPath: str = f"{_workingDirectoryPath}{LOGGER_LOGFOLDER}/"
 
 
 class CansatLogger:
-    _logMainName: str = ""
+    _logFileMainName: str = ""
 
     def __init__(self):
-        self._logMainName = f"{_logFolderPath}{LOGGER_LOGMAINNAME}{len(os.listdir(_logFolderPath)) + 1}.csv"
+        self._logFileMainName = f"{_logFolderPath}{LOGGER_LOGMAINNAME}{len(os.listdir(_logFolderPath)) + 1}.csv"
 
     def LogData(self, *data: any):
         logStringData: str = ""
 
         for i in range(0, len(data)):
-            logStringData += str(data[i]) + (";" if i < len(data) - 1 else "\n")
+            logStringData += str(data[i]) + (LOGGER_CSVSEPERATOR if i < len(data) - 1 else "\n")
 
-        with open(self._logMainName, "a") as logFile:
+        with open(self._logFileMainName, "a") as logFile:
             logFile.write(logStringData)
-
-
-
