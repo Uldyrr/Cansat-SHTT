d2ed390e6865eea89b79033c1584403fc8702090
tommy <tommyroslik420@gmail.com>
2024-04-10 15:40:21 +0200

    Update

--- Raspberry Pi Pico/src/CansatCore.py
+++ Raspberry Pi Pico/src/CansatCore.py
@@ -24,7 +24,7 @@ class _sensors:
 class _components:
     BuiltInLed: Pin = Pin(25, Pin.OUT)
     AlarmBuzzer: Pin = Pin(13, Pin.OUT)
-    StatusLed: Pin = Pin(2, Pin.OUT)
+    StatusLed: Pin = Pin(0, Pin.OUT)
 
 
 # // Constants
